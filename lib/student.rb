abadeb046b0d96e5cdb6256b3e65c02594d9b9a6
Tony Baker <this.tonybaker@gmail.com>
2019-05-21 22:00:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,18 @@ class Student
     end
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < ?
+    SQL
+
+    DB[:conn].execute(sql, 12).map do |row|
+      self.new_from_db
+    end
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
