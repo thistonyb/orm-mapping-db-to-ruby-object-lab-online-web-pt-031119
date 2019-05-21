33de22604baf289f385375ecfd77155eb90ee551
Tony Baker <this.tonybaker@gmail.com>
2019-05-21 21:54:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,10 +24,10 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 9
+      WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end
   end
